10710b70174dc37a8fd39de081292f0b05fff733
Dencho Tsanov <d.tsanov@atomsolutions.net>
2019-04-11 16:44:32 +0300

    create user table

---
+++ migrations/m190411_134113_create_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ */
+class m190411_134113_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string(255),
+            'password' => $this->string(255),
+            'email' => $this->string(255),
+            'access_token' => $this->string(255),
+            'forgotten_password_token' => $this->string(255),
+            'updated_at' => $this->integer(),
+            'created_at' => $this->integer()
+
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
