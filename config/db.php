2cb95fefa17ae15a5e1f26f7d6fb2ad41dbd0c8a
Dencho Tsanov <tsanov.dencho@gmail.com>
2019-07-27 17:12:33 +0300

    update updated_at

--- config/db.php
+++ config/db.php
@@ -2,9 +2,6 @@
 
 return [
     'class' => 'yii\db\Connection',
-//    'dsn' => 'mysql:host=localhost;dbname=rawllybg_calc',
-//    'username' => 'rawllybg_calc',
-//    'password' => 'p{*}?z.K*13C',
     'dsn'=>'mysql:host=localhost;dbname=rawlly',
     'username' => 'root',
     'password' => '',
